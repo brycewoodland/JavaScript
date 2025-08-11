1d1190082b41bc25e78af2d9e2643b04a19cc811
Bryce Woodland <bwoodland@vivacitytech.com>
2025-08-10 22:29:34 -0600

    Built a class for Binary Tree

--- dfsPractice.js
+++ dfsPractice.js
@@ -1,42 +1,85 @@
-// Max Depth
-const maxDepth = (root) => {
-    if (!root) {
-        return 0;
+class BinaryTree {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 
-    const leftDepth = maxDepth(root.left);
-    const rightDepth = maxDepth(root.right);
-
-    return 1 + Math.max(leftDepth, rightDepth);
-};
-
-// Path Sum
-const pathSum = (root, target) => {
-    if (!root) {
-        return false;
-    }
-
-    if (!root.left && !root.right) {
-        return target === root.val;
-    }
-
-    target -= root.val;
-
-    return pathSum(root.left, target) || pathSum(root.right, target);
-};
-
-// Validate BST
-function validateBST(root) {
-    function isValid(root, min, max) {
+    // Max Depth
+    maxDepth(root) {
         if (!root) {
-            return true;
+            return 0;
         }
 
-        if (root.val <= min || root.val >= max) {
+        const leftDepth = this.maxDepth(root.left);
+        const rightDepth = this.maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    };
+
+    // Path Sum
+    pathSum = (root, target) => {
+        if (!root) {
             return false;
         }
 
-        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
-    }
-    return isValid(root, -Infinity, Infinity);
+        if (!root.left && !root.right) {
+            return target === root.val;
+        }
+
+        target -= root.val;
+
+        return this.pathSum(root.left, target) || this.pathSum(root.right, target);
+    };
+
+    // Validate BST
+    validateBST(root) {
+        const isValid = (root, min, max) => {
+            if (!root) {
+                return true;
+            }
+
+            if (root.val <= min || root.val >= max) {
+                return false;
+            }
+
+            return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+        }
+        return isValid(root, -Infinity, Infinity);
+    };
+
+    // Good Nodes
+    goodNodes(root) {
+        const isGood = (root, maxSoFar) => {
+            if (!root) {
+                return 0
+            }
+
+            let count = 0;
+            if (root.val >= maxSoFar) {
+                count += 1;
+                maxSoFar = root.val;
+            }
+
+            const left = isGood(root.left, maxSoFar);
+            const right = isGood(root.right, maxSoFar);
+
+            return count + left + right;
+        }
+        return isGood(root, -Infinity);
+    };
 }
+
+const root = new BinaryTree(5);
+root.left = new BinaryTree(3);
+root.right = new BinaryTree(7);
+
+root.left.left = new BinaryTree(2);
+root.left.right = new BinaryTree(4);
+
+root.right.left = null;
+root.right.right = new BinaryTree(9);
+
+console.log('Good Nodes Count:', root.goodNodes(root));
+
+console.log('Is Valid BST?', root.validateBST(root));
